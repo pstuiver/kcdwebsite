0533c549059d99ec9e12a3d9d1bb60457b753b78
pstuiver <pstuiverx@gmail.com>
2023-07-31 23:36:24 +0200

    add pages:'build" to svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,9 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ pages: 'build', assets: 'build' }),
 		paths: {
-			base: dev ? '' : '/www.kidzcandurban.com'
+			base: dev ? '' : process.env.BASE_PATH
 		}
 	},
 
