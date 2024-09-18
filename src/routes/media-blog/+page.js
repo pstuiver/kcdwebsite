8fe59b44b874b2e374ca856458159011a650ed47
pstuiver <pstuiverx@gmail.com>
2024-09-18 11:02:26 +0200

    update dependencies+format+lint

--- src/routes/media-blog/+page.js
+++ src/routes/media-blog/+page.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load() {
-	let blogSRC = 'https://kidzcandurban.blogspot.com';
+  let blogSRC = "https://kidzcandurban.blogspot.com";
 
-	return { blogSRC };
+  return { blogSRC };
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const dev = process.argv.includes('dev');
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({ pages: 'build', assets: 'build' }),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		}
-	},
+  kit: {
+    adapter: adapter({ pages: "build", assets: "build" }),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH,
+    },
+  },
 
-	preprocess: [vitePreprocess({})]
+  preprocess: [vitePreprocess({})],
 };
 
 export default config;
