9a8a0d4c4cc990ddb33e171b86e8c9faaea7aaa2
pstuiver <pstuiverx@gmail.com>
2024-10-24 14:08:29 +0200

    Update deps for Svelte 5

--- src/routes/media-blog/+page.svelte
+++ src/routes/media-blog/+page.svelte
@@ -1,23 +1,24 @@
 <script>
-	import RouteHead from '$lib/RouteHead.svelte';
-	const headerColor ='n50-on-blue';
-	const headerTitle ='Blog Posts';
-	export let data;
-	const { blogSRC } = data;
+  import RouteHead from "$lib/RouteHead.svelte";
+  const headerColor = "n50-on-blue";
+  const headerTitle = "Blog Posts";
+  export let data;
+  const { blogSRC } = data;
 </script>
 
 <svelte:head>
-	<title>KidzCanDurban-Blog Posts</title>
+  <title>KidzCanDurban-Blog Posts</title>
 </svelte:head>
 
 <div class="views-container">
-	<RouteHead {headerColor} {headerTitle} />
+  <RouteHead {headerColor} {headerTitle} />
 
-	<div class="flex flex-col items-center relative w-full h-screen m-0 p-0">
-		<div
-			class="absolute origin-top width-full h-screen scale-[0.43] xs:scale-[0.66]
+  <div class="flex flex-col items-center relative w-full h-screen m-0 p-0">
+    <div
+      class="absolute origin-top width-full h-screen scale-[0.43] xs:scale-[0.66]
 			md:scale-[1.0] -mt-16 xs:-mt-32 md:-mt-48">
-			<iframe title="kidzcandurban blog" width="780px" height="250%" class="" src={blogSRC} />
-		</div>
-	</div>
+      <iframe title="kidzcandurban blog" width="780px" height="250%" class="" src={blogSRC}
+      ></iframe>
+    </div>
+  </div>
 </div>
